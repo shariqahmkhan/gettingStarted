bfd2c612e6568c6ba95dc9889513c80ab84ef643
Shariq Ahmed Khan <shariqahmkhan@gmail.com>
2020-12-15 12:11:58 +0530

    created basic file

---
+++ first/first.py
@@ -0,0 +1,9 @@
+import sys
+print("type something in commnd line;", sys.argv[0])
+# cd folder
+print('\n\n a random path is', sys.path, '.\n')
+# considered for faster i/o
+user_input = sys.stdin.readline()
+print(user_input)
+
+
